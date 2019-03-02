974e35d2b8f5485c3ac4f1b624326db8e7971e94
Michael Kaufman <mkaufman94@gmail.com>
2019-03-02 13:33:34 -0500

    fix: Load data in scenarios

--- mirage/config.js
+++ mirage/config.js
@@ -1,11 +1,9 @@
 export default function() {
-  const server = this;
   this.loadFixtures();
 
   this.namespace = '/api';
 
   this.get('/gradezilla/v1/assignments', (schema, request) => {
-    server.createList('assignment', 5);
     let assignments = schema.assignments.all();
     return {
       "assignments": assignments.models
