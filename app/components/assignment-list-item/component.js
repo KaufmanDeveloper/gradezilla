06729a0b75ce23c6ea9cb5a1faf3bd98e4a2e32b
Michael Kaufman <mkaufman94@gmail.com>
2019-03-03 09:54:33 -0500

    fix: Clean up assignments display

--- app/components/assignment-list-item/component.js
+++ app/components/assignment-list-item/component.js
@@ -1,8 +1,21 @@
 import Component from '@ember/component';
 
+import {
+  computed
+} from '@ember/object';
+
 export default Component.extend({
   tagName: 'a',
   classNames: ['item'],
+
+  descriptionLimited: computed(function() {
+    const description = this.get('assignment.description');
+    let descriptionLimited = description.substring(1, 40);
+    if (description.length > 40) {
+      descriptionLimited = descriptionLimited + "..."
+    }
+    return descriptionLimited;
+  }),
 
   click() {
     this.set('isOpen', true)
