1b90c0ad3b9da5e0fefb343967c802570bf1ca6d
Michael Kaufman <mkaufman94@gmail.com>
2019-04-15 11:01:31 -0400

    fix: Remove unused import

--- app/components/class-selector/component.js
+++ app/components/class-selector/component.js
@@ -3,9 +3,6 @@ import Component from '@ember/component';
 import {
   inject as service
 } from '@ember/service';
-import {
-  task,
-} from 'ember-concurrency';
 import { alias } from '@ember/object/computed';
 
 export default Component.extend({
